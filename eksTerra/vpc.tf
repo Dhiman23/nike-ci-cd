5f5ca3713211db6d8f64eb7cee1210a6aeea236f
Dhiman23 <sajaldhiman68@gmail.com>
2024-08-05 23:52:36 +0530

    added vpc and data

---
+++ eksTerra/vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-cluster"
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availablity_zones.azs.names 
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+
+  }
+  public_subnet_tags = {
+    "kubenetes.io/cluster/eks-cluster" = "shared"
+    "kubenetes.io/role/elb"            = 1
+  }
+
+}
