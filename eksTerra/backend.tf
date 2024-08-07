57c523507da7564d86270195ba537f8ba6bbeff8
Dhiman23 <sajaldhiman68@gmail.com>
2024-08-07 15:46:47 +0530

    added

--- eksTerra/backend.tf
+++ eksTerra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terra-bucket-23"
-    key    = "eks-nike/terraform.tfstate"
+    key    = "eks-nike1/terraform.tfstate"
     region = "us-east-1"
   }
 }
