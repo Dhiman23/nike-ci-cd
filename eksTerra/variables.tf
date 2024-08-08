ac239a8cb9244a1746e05eda337e3f7a4f775b46
Dhiman23 <sajaldhiman68@gmail.com>
2024-08-08 16:41:59 +0530

    changed

--- eksTerra/variables.tf
+++ eksTerra/variables.tf
@@ -1,19 +1,13 @@
+variable "kubernetes_version" {
+  default     = 1.29
+  description = "kubernetes version"
+}
+
 variable "vpc_cidr" {
-  description = "VPC CIDR"
-  type = string
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
 }
-
-variable "public_subnets" {
-  description = "public_subnets CIDR"
-  type = list(string)
-}
-
-variable "private_subnets" {
-  description = "private_subnets CIDR"
-  type = list(string)
-}
-
-variable "instance_types" {
-  description = "Node Instance"
-  type = list(string)
+variable "aws_region" {
+  default = "us-west-1"
+  description = "aws region"
 }
