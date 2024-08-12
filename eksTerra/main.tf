fd7bcab38b88d4b85454a05d8c9e81a591682c7a
Dhiman23 <sajaldhiman68@gmail.com>
2024-08-12 23:42:36 +0530

    added

--- eksTerra/main.tf
+++ eksTerra/main.tf
@@ -1,57 +1,84 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "eks-vpc"
-  cidr = var.vpc_cidr
+  name = "my-demo-vpc"
+  cidr = "10.10.0.0/16"
 
-  azs = data.aws_availability_zones.azs.names
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
 
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+ map_public_ip_on_launch = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    Terraform = "true"
+    Environment = "dev"
   }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"      = 1
-  }
-
 }
 
+
+
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-
-  cluster_name    = "eks-cluster-v1"
+  cluster_name    = "my-cluster"
   cluster_version = "1.29"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    nodes = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 2
-
-      instance_type = ["t3.medium"]
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
+  vpc_id                   = module.vpc.vpc_id
+  
+  control_plane_subnet_ids = module.vpc.public_subnets
+
+  subnet_ids               = module.vpc.public_subnets
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.large"]
+  }
+
+  eks_managed_node_groups = {
+    example = {
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
   tags = {
     Environment = "dev"
     Terraform   = "true"
